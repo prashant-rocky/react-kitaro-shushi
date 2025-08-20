1589751bec70d64482ace06be1f21b63c6b9c825
prashant-rocky <p3532490@gmail.com>
2025-08-20 18:25:31 +0530

    again logo change

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -3,9 +3,9 @@ import logo from "../../assets/logo.png";
 import { Link } from "react-router-dom";
 
 const Footer = () => {
-    const Scroll = () => {
-        window.scrollTo(0, 0);
-    }
+  const Scroll = () => {
+    window.scrollTo(0, 0);
+  }
   useEffect(() => {
     // Load TermsFeed cookie script dynamically
     const script = document.createElement("script");
@@ -35,126 +35,126 @@ const Footer = () => {
   }, []);
 
   return (
-      <footer className="bg-body-tertiary pt-5">
-        <div className="container">
-          <div className="row">
-            {/* Logo + Description */}
-            <div className="col-md-4">
-              <div className="footerLogo">
-                <img src={logo} alt="Hakata Kitaro Sushi Logo" className="pb-3" />
-                <p>
-                  Hakata Kitaro Sushi welcomes you to experience authentic
-                  Japanese dining in the heart of Fukuoka. From traditional sushi
-                  to chef’s specials, we take pride in serving fresh flavors with
-                  warm hospitality.
-                </p>
-                <p>
-                  Japan, 〒810-0004 Fukuoka, Chuo Ward, Watanabedori, 5
-                  Chome−2−9 1F
-                </p>
-                <p>Phone: +81 92-406-8682</p>
-                <p>Email: info@example.com</p>
-              </div>
-            </div>
-
-            {/* Useful Links */}
-            <div className="col-md-4 px-md-5">
-              <h4 className="py-3">Useful Links</h4>
+    <footer className="bg-body-tertiary pt-5">
+      <div className="container">
+        <div className="row">
+          {/* Logo + Description */}
+          <div className="col-md-4">
+            <div className="footerLogo">
+              <img src={logo} alt="Hakata Kitaro Sushi Logo" className="pb-3" />
               <p>
-                <a href="#" className="text-decoration-none text-black">
-                  Home
-                </a>
+                Hakata Kitaro Sushi welcomes you to experience authentic
+                Japanese dining in the heart of Fukuoka. From traditional sushi
+                to chef’s specials, we take pride in serving fresh flavors with
+                warm hospitality.
               </p>
               <p>
-                <a href="#" className="text-decoration-none text-black">
-                  About
-                </a>
+                Japan, 〒810-0004 Fukuoka, Chuo Ward, Watanabedori, 5
+                Chome−2−9 1F
               </p>
-              <p>
-                <a href="#" className="text-decoration-none text-black">
-                  Menu
-                </a>
-              </p>
-              <p>
-                <a href="#" className="text-decoration-none text-black">
-                  Blog
-                </a>
-              </p>
-              <p>
-                <a href="#" className="text-decoration-none text-black">
-                  Contact
-                </a>
-              </p>
-            </div>
-
-            {/* Opening Hours */}
-            <div className="col-md-4">
-              <h4 className="py-3">Opening Hours</h4>
-              <p>Monday - Friday: 11:00 AM - 10:00 PM</p>
-              <p>Saturday - Sunday: 12:00 PM - 11:00 PM</p>
-              <p>Holidays: Closed</p>
+              <p>Phone: +81 92-406-8682</p>
+              <p>Email: info@example.com</p>
             </div>
           </div>
 
-          {/* Bottom Row */}
-          <div className="row">
-            <div className="col-md-6">
-              <div className="text-center py-3">
-                <p className="mb-0">
-                  © 2023 Hakata Kitaro Sushi. All rights reserved.
-                </p>
-              </div>
+          {/* Useful Links */}
+          <div className="col-md-4 px-md-5">
+            <h4 className="py-3">Useful Links</h4>
+            <p>
+              <a href="#" className="text-decoration-none text-black">
+                Home
+              </a>
+            </p>
+            <p>
+              <a href="#" className="text-decoration-none text-black">
+                About
+              </a>
+            </p>
+            <p>
+              <a href="#" className="text-decoration-none text-black">
+                Menu
+              </a>
+            </p>
+            <p>
+              <a href="#" className="text-decoration-none text-black">
+                Blog
+              </a>
+            </p>
+            <p>
+              <a href="#" className="text-decoration-none text-black">
+                Contact
+              </a>
+            </p>
+          </div>
+
+          {/* Opening Hours */}
+          <div className="col-md-4">
+            <h4 className="py-3">Opening Hours</h4>
+            <p>Monday - Friday: 11:00 AM - 10:00 PM</p>
+            <p>Saturday - Sunday: 12:00 PM - 11:00 PM</p>
+            <p>Holidays: Closed</p>
+          </div>
+        </div>
+
+        {/* Bottom Row */}
+        <div className="row">
+          <div className="col-md-4">
+            <div className="text-center py-3">
+              <p className="mb-0">
+                © 2023 Hakata Kitaro Sushi. All rights reserved.
+              </p>
             </div>
-            <div className="col-md-6 py-3 text-center">
-              <span>
-                <Link
-                  to="/cookies"
-                  className="text-decoration-none text-black px-2"
-                  onClick={Scroll}
-                >
-                  Cookies Policy
-                </Link>
-                |
-                <Link
-                  to="/privacy"
-                  className="text-decoration-none text-black px-2"
-                  onClick={Scroll}
-                >
-                  Privacy Policy
-                </Link>
-                |
-                <Link
-                  to="/disclaimer"
-                  className="text-decoration-none text-black px-2"
-                  onClick={Scroll}
-                >
-                  Disclaimer
-                </Link>
-                |
-                <Link
-                  to="/terms&conditions"
-                  className="text-decoration-none text-black px-2"
-                  onClick={Scroll}
-                >
-                  Terms & Conditions
-                </Link>
-              </span>
+          </div>
+          <div className="col-md-8 py-3 text-center">
+            <span>
+              <Link
+                to="/cookies"
+                className="text-decoration-none text-black px-2"
+                onClick={Scroll}
+              >
+                Cookies Policy
+              </Link>
+              |
+              <Link
+                to="/privacy"
+                className="text-decoration-none text-black px-2"
+                onClick={Scroll}
+              >
+                Privacy Policy
+              </Link>
+              |
+              <Link
+                to="/disclaimer"
+                className="text-decoration-none text-black px-2"
+                onClick={Scroll}
+              >
+                Disclaimer
+              </Link>
+              |
+              <Link
+                to="/terms&conditions"
+                className="text-decoration-none text-black px-2"
+                onClick={Scroll}
+              >
+                Terms & Conditions
+              </Link>
+            </span>
+            {/* Update cookies preferences link */}
+            <div className="text-center py-2 d-inline">
+              <a
+                href="#"
+                id="open_preferences_center"
+                className="text-decoration-none text-black"
+              >
+                Update cookies preferences
+              </a>
             </div>
           </div>
         </div>
-      {/* Update cookies preferences link */}
-      <div className="text-center py-2">
-        <a
-          href="#"
-          id="open_preferences_center"
-          className="text-decoration-none text-black"
-        >
-          Update cookies preferences
-        </a>
       </div>
-      </footer>
+    </footer>
 
-   
+
   );
 };
 
